45dd7337ff977e7f222f7363547379c115ffc28b
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2021-05-17 09:18:56 -0300

    feat: created .h header file

---
+++ src/HtmlManager.h
@@ -0,0 +1,17 @@
+/**
+ * @file HtmlManager.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-05-17
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __HTMLMANAGER__H__
+#define __HTMLMANAGER__H__
+
+#include "HtmlManager.hpp"
+
+#endif  //!__HTMLMANAGER__H__
